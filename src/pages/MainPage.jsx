4fe3b6cda71bcbf1a7ebeb8f851bd55d9d6a7517
junjangsee <junjang.see@gmail.com>
2020-11-02 00:15:53 +0900

    [#13]feat: 피드 컴포넌트 구현

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Header from '../components/Header';
 import Story from '../components/main/Story';
+import Feed from '../components/main/Feed';
 import profile from '../shared/__mocks__/profile';
+import feeds from '../shared/__mocks__/feeds';
 
 class MainPage extends React.Component {
   render() {
@@ -13,6 +15,19 @@ class MainPage extends React.Component {
             <div className='main'>
               <div className='contents'>
                 <Story />
+                <div className='feed-wrapper'>
+                  {feeds.map((feed, index) => (
+                    <Feed
+                      key={index}
+                      thumbnail={feed.thumbnail}
+                      nickname={feed.nickname}
+                      likes={feed.likes}
+                      nickname={feed.nickname}
+                      content={feed.content}
+                      comments={feed.comments}
+                      date={feed.date}></Feed>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
