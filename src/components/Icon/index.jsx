89e8873f25e56b97df903bacd897be2009e1ca52
junjangsee <junjang.see@gmail.com>
2020-11-02 14:18:55 +0900

    [#27]refactor: 아이콘 이미지 추가 및 컴포넌트 적용

--- src/components/Icon/index.jsx
+++ src/components/Icon/index.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
-import { device, instagram_images, home, home_active } from '../../images';
+import {
+  device,
+  instagram_images,
+  home,
+  home_active,
+  comment,
+  comment_active,
+  direct,
+  direct_active,
+  explore,
+  explore_active,
+  feed,
+  feed_active,
+  heart,
+  heart_active,
+  like,
+  like_active,
+  save,
+  save_active,
+  setting,
+  trans,
+  option,
+  share,
+  close,
+} from '../../images';
 
 class Icon extends React.Component {
   render() {
-    const { type, isToggle, width = 22, height = 22 } = this.props;
+    const { type, isToggle = false, width = 22, height = 22 } = this.props;
     let src = '';
     let typeString = `${type}${isToggle ? '_active' : ''}`;
 
@@ -19,6 +43,62 @@ class Icon extends React.Component {
         break;
       case 'home_active':
         src = home_active;
+        break;
+      case 'comment':
+        src = comment;
+        break;
+      case 'comment_active':
+        src = comment_active;
+        break;
+      case 'direct':
+        src = direct;
+        break;
+      case 'direct_active':
+        src = direct_active;
+        break;
+      case 'explore':
+        src = explore;
+        break;
+      case 'explore_active':
+        src = explore_active;
+        break;
+      case 'heart':
+        src = heart;
+        break;
+      case 'heart_active':
+        src = heart_active;
+        break;
+      case 'feed':
+        src = feed;
+        break;
+      case 'feed_active':
+        src = feed_active;
+      case 'like':
+        src = like;
+        break;
+      case 'like_active':
+        src = like_active;
+        break;
+      case 'save':
+        src = save;
+        break;
+      case 'save_active':
+        src = save_active;
+        break;
+      case 'setting':
+        src = setting;
+        break;
+      case 'trans':
+        src = trans;
+        break;
+      case 'option':
+        src = option;
+        break;
+      case 'share':
+        src = share;
+        break;
+      case 'close':
+        src = close;
         break;
     }
 
--- src/images/index.js
+++ src/images/index.js
@@ -8,6 +8,8 @@ import direct from './direct.svg';
 import direct_active from './direct_active.svg';
 import explore from './explore.svg';
 import explore_active from './explore_active.svg';
+import feed from './feed.svg';
+import feed_active from './feed_active.svg';
 import heart from './heart.svg';
 import heart_active from './heart_active.svg';
 import like from './like.svg';
@@ -16,6 +18,9 @@ import save from './save.svg';
 import save_active from './save_active.svg';
 import setting from './setting.svg';
 import trans from './trans.svg';
+import option from './option.svg';
+import share from './share.svg';
+import close from './close.svg';
 
 export {
   device,
@@ -28,6 +33,8 @@ export {
   direct_active,
   explore,
   explore_active,
+  feed,
+  feed_active,
   heart,
   heart_active,
   like,
@@ -36,4 +43,7 @@ export {
   save_active,
   setting,
   trans,
+  option,
+  share,
+  close,
 };
