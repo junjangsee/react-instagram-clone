7ca37385692d7c3f0a3830c6314c7ce5a63b89b0
junjangsee <junjang.see@gmail.com>
2020-11-02 15:54:35 +0900

    [#33]fix: className 추가

--- src/components/Image/index.jsx
+++ src/components/Image/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import style from './index.module.scss';
+import classNames from 'classnames';
 
 class Image extends React.Component {
   render() {
     const { src, alt, className, width, height, margin, padding } = this.props;
+    const classProps = classNames(style.image, className);
 
     return (
       <img
         src={src}
-        className={className}
+        className={classProps}
         style={{ width, height, margin, padding }}
         alt={alt}
       />
