608534b15856a17a3e8c8e682ca32dd553973f34
junjangsee <junjang.see@gmail.com>
2020-11-02 12:08:03 +0900

    [#25]setup/ 아이콘 이미지 추가

--- src/images/index.js
+++ src/images/index.js
@@ -2,5 +2,38 @@ import device from './device.png';
 import instagram_images from './instagram-images.png';
 import home from './home.svg';
 import home_active from './home_active.svg';
+import comment from './comment.svg';
+import comment_active from './comment_active.svg';
+import direct from './direct.svg';
+import direct_active from './direct_active.svg';
+import explore from './explore.svg';
+import explore_active from './explore_active.svg';
+import heart from './heart.svg';
+import heart_active from './heart_active.svg';
+import like from './like.svg';
+import like_active from './like_active.svg';
+import save from './save.svg';
+import save_active from './save_active.svg';
+import setting from './setting.svg';
+import trans from './trans.svg';
 
-export { device, instagram_images, home, home_active };
+export {
+  device,
+  instagram_images,
+  home,
+  home_active,
+  comment,
+  comment_active,
+  direct,
+  direct_active,
+  explore,
+  explore_active,
+  heart,
+  heart_active,
+  like,
+  like_active,
+  save,
+  save_active,
+  setting,
+  trans,
+};
