6c6498f56b3eda4b4c48badecfe50ad8740d09e8
junjangsee <junjang.see@gmail.com>
2020-11-02 11:11:36 +0900

    [#23]feat: Icon 공통 컴포넌트 구현

---
+++ src/images/index.js
@@ -0,0 +1,6 @@
+import device from './device.png';
+import instagram_images from './instagram-images.png';
+import home from './home.svg';
+import home_active from './home_active.svg';
+
+export { device, instagram_images, home, home_active };
